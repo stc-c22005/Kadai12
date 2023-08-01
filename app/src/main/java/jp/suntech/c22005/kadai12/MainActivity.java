a1d393fe5fa1d211bc5ddff6497ef334b5b3d847
stc-c22005 <c22005@suntech.jp>
2023-08-01 09:59:20 +0900

    Fifth commit

--- app/src/main/java/jp/suntech/c22005/kadai12/MainActivity.java
+++ app/src/main/java/jp/suntech/c22005/kadai12/MainActivity.java
@@ -1,7 +1,6 @@
 package jp.suntech.c22005.kadai12;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
